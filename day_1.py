6553437432bb953a5337befc940818e3ce5a0cdc
Aswinnidesh.A <aswinnidesh@gmail.com>
2026-02-17 09:53:18 +0530

    Add files via upload
    day 1

---
+++ day_1.py
@@ -0,0 +1,86 @@
+# _apple = 10 
+# print(_apple)
+
+# # Data Types
+# # int,float,bool,str
+# yob = 2005 
+
+# current_year=2026 
+# age=current_year-yob
+# print("Your age=",age)
+
+# current_age=21
+# max_age=26
+# estimated_amount=300
+# per_year=estimated_amount*365
+# years_left=max_age-current_age
+# print("you will need ",per_year*years_left,"to last till old age ")
+
+# current_age=21
+# max_age=26
+# liters_perday=3
+# age_left=max_age-current_age
+# print("years left to live ",age_left)
+# days=age_left*365
+# total=days*liters_perday
+# print("you will dirnk",total,"liters for rest of your life")
+
+# total_trip=1000
+# car_mileage=20
+# petrol_price=100
+# liters_need=1000/20
+# print("fuel needed =",liters_need)
+# total_cost=liters_need*petrol_price
+# print("total cost for the tirp=",total_cost)
+
+# daily_data_usage=5
+# no_of_days=31
+# total=daily_data_usage*no_of_days
+# print("you use ",total,"GB of data per month")
+
+# current_age=21
+# years_into_days=current_age*365
+# print("you are approximately",years_into_days,"days old")
+
+# org_price=100
+# discount=10
+# discount_amount=discount/100*org_price
+# print("discount_amount=",discount_amount)
+# final_price=org_price-discount_amount
+# print("Final price =",final_price)
+
+# x=int(input("enter the value ="))
+# y=int(input("enter the value ="))
+# if(x==y):
+#     print("x and y are equal")
+# elif(x>y):
+#     print("x is greater y ")
+# else:
+#     print("x is less than y")
+
+variable=input("enter a character =")
+if(variable==str("a" or "e" or "i" or "o" or "u")):
+    print("vowels")
+else:
+    print("consoant")
+
+# mark=int(input("enter the  mark="))   
+# if (mark==100):
+#     print("Grade = S")
+# elif(mark<99 and mark>80):
+#     print("Grade = A")
+# elif(mark<80 and mark>60):
+#     print("Grade is B")
+# elif(mark<60 and mark>50 ):
+#     print("Grade is c")
+# else:
+#     print("F")    
+
+buying_price=float(input("enter the price"))
+selling_price=float(input("enter the price"))
+if(buying_price>selling_price):
+    print("lose")
+elif(selling_price==buying_price):
+    print("no loss no profit")    
+else:
+    print("profit")    
