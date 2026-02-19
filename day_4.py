0967f9f502351fe111a33a5d867fa0a0c239ab39
Aswinnidesh.A <aswinnidesh@gmail.com>
2026-02-19 16:10:46 +0530

    Add files via upload

---
+++ day_4.py
@@ -0,0 +1,54 @@
+# l1=[1,4,5]
+# l2=[2,3,4]
+# print(l1+l2)
+# m=list(set(l1+l2))
+# print(m)
+# n=list(dict.fromkeys(l1+l2))
+# print(n)
+
+# a=input()
+# b=input()
+# if(len(a)==len(b)):
+#     n=(sorted(a))
+#     m=(sorted(b))
+#     if(n==m):
+#         print("anagram")
+#     else:
+#         print("not anagram ")
+
+# list1 = [1,5,4,3] 
+# list2 = [1,2,3,4]
+# m= list1 + list2
+# m.sort()
+# print(m)
+
+# n=int(input("enter the vaule="))
+# if(n==0):
+#     print("zero")
+# elif(n>0):
+#     print("positive")
+# else:
+#     print("negative")        
+
+# a="hello world"
+# a.strip()
+# a.split()
+# print(len(a))
+
+# from typing import List
+
+# class Solution:
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         return list(set(nums1) & set(nums2))
+# nums1 = [1,2,2,1]
+# nums2 = [2,2]
+
+# l1=[1,4,2]
+# l2=[3,2,5]
+# print(l1+l2)
+
+# a=["h","e","l","l","o"]
+# print(list(a[::-1]))
+
+# n=45678
+# print(1+(n-1)%9)
